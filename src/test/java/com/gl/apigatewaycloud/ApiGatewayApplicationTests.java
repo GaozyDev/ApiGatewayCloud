4973b8b6d67dab7bd3bdf51678be358fdd40a1fa
GaozyDev <670453367@qq.com>
2021-05-14 14:10:00 +0800

    init

---
+++ src/test/java/com/gl/apigatewaycloud/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gl.apigatewaycloud;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
